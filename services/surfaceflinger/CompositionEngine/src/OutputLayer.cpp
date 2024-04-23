vroad/android_frameworks_native

surfaceflinger: Provide layer names and stuffs

services/surfaceflinger/CompositionEngine/src/OutputLayer.cpp
@@ -348,6 +348,12 @@ void OutputLayer::writeStateToHWC(bool includeGeometry) const {
                   to_string(error).c_str(), static_cast<int32_t>(error));
         }
 
+        if (auto error = hwcLayer->setLayerName(mLayerFE->getDebugName());
+            error != HWC2::Error::None) {
+            ALOGE("[%s] Failed to set layer name: %s (%d)", mLayerFE->getDebugName(),
+                  to_string(error).c_str(), static_cast<int32_t>(error));
+        }
+
         if (auto error =
                     hwcLayer->setTransform(static_cast<HWC2::Transform>(mState.bufferTransform));
             error != HWC2::Error::None) {

openfde/lineageos_android_frameworks_native

services/surfaceflinger/CompositionEngine/src/OutputLayer.cpp
@@ -383,6 +383,12 @@ void OutputLayer::writeOutputDependentGeometryStateToHWC(
               outputDependentState.z, to_string(error).c_str(), static_cast<int32_t>(error));
     }
 
+    if (auto error = hwcLayer->setLayerName(getLayerFE().getDebugName());
+            error != hal::Error::NONE) {
+            ALOGE("[%s] Failed to set layer name: %s (%d)", getLayerFE().getDebugName(),
+                  to_string(error).c_str(), static_cast<int32_t>(error));
+    }
+
     // Solid-color layers should always use an identity transform.
     const auto bufferTransform = requestedCompositionType != hal::Composition::SOLID_COLOR
             ? outputDependentState.bufferTransform