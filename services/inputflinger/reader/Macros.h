17bcc61d12df252cd4c01cd587f90bbd410be6b4
ironlvxyy <ironmanpeng@126.com>
2025-08-01 14:05:22 +0800

    enable log of RAW_EVENTS's

--- services/inputflinger/reader/Macros.h
+++ services/inputflinger/reader/Macros.h
@@ -19,10 +19,10 @@
 
 #define LOG_TAG "InputReader"
 
-//#define LOG_NDEBUG 0
+#define LOG_NDEBUG 0
 
 // Log debug messages for each raw event received from the EventHub.
-#define DEBUG_RAW_EVENTS 0
+#define DEBUG_RAW_EVENTS 1
 
 // Log debug messages about touch screen filtering hacks.
 #define DEBUG_HACKS 0
